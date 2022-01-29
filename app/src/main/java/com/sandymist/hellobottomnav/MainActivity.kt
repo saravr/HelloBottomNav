6f171b07c44920cef7540dd69ece18de98a15366
Sarav Ramaswamy <sarav@verizonmedia.com>
2022-01-29 00:22:27 -0800

    Add fragment item

--- app/src/main/java/com/sandymist/hellobottomnav/MainActivity.kt
+++ app/src/main/java/com/sandymist/hellobottomnav/MainActivity.kt
@@ -6,29 +6,40 @@ import android.view.Menu
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+data class FragmentItem(
+    val label: String,
+    val iconRes: Int,
+    val fragmentClass: Class<out Fragment>
+)
+
 class MainActivity : AppCompatActivity() {
-    private val fragmentClassList = mutableListOf<Class<out Fragment>>()
+    private val fragmentItemList = mutableListOf<FragmentItem>()
     private var currentFragment: Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fragmentClassList.addAll(listOf(HomeFragment::class.java, SettingsFragment::class.java))
+        fragmentItemList.addAll(
+            listOf(
+                FragmentItem("Home", R.drawable.ic_home, HomeFragment::class.java),
+                FragmentItem("Settings", R.drawable.ic_settings, SettingsFragment::class.java)
+            )
+        )
 
         loadFragment(0)
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
         bottomNavigationView?.menu?.apply {
-            add(Menu.NONE, 0, 0, "Home")
-                .setIcon(R.drawable.ic_home)
-            add(Menu.NONE, 1, 1, "Settings")
-                .setIcon(R.drawable.ic_settings)
+            fragmentItemList.forEachIndexed { index, fragmentItem ->
+                add(Menu.NONE, index, index, fragmentItem.label)
+                    .setIcon(fragmentItem.iconRes)
+            }
         }
 
         bottomNavigationView?.setOnItemSelectedListener {
-            fragmentClassList.getOrNull(it.itemId)?.let { fragment ->
-                loadFragment(fragmentClassList.indexOf(fragment))
+            fragmentItemList.getOrNull(it.itemId)?.let { fragment ->
+                loadFragment(fragmentItemList.indexOf(fragment))
             }
             false
         }
@@ -37,7 +48,7 @@ class MainActivity : AppCompatActivity() {
     private fun loadFragment(index: Int) {
         val transaction = supportFragmentManager.beginTransaction()
 
-        val fragmentClass = fragmentClassList.getOrNull(index)
+        val fragmentClass = fragmentItemList.getOrNull(index)?.fragmentClass
         val tag = fragmentClass?.simpleName
 
         var nextFragment = supportFragmentManager.findFragmentByTag(tag)
